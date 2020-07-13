1186291dd44cb36fdb27995b7c333dffd98917e6
nguyendoan97 <nguyenhongdoan14@gmail.com>
2020-07-13 16:10:18 +0700

    fix lỗi tạo bảng blog

--- src/main/java/com/casestudy/casestudy/models/blogs/Post.java
+++ src/main/java/com/casestudy/casestudy/models/blogs/Post.java
@@ -1,15 +1,15 @@
 package com.casestudy.casestudy.models.blogs;
 
 
-
-
 import com.casestudy.casestudy.models.Users;
+import lombok.Data;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
 
 @Entity
 @Table(name = "blogs")
+@Data
 public class Post {
 
     @Id
@@ -21,71 +21,13 @@ public class Post {
     private String content;
     private Timestamp date_create = new Timestamp(System.currentTimeMillis());
 
-    @Column(columnDefinition = " default 0")
+    @Column(columnDefinition = "Integer default 0")
     private Long likes;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private Users users_id;
 
     @ManyToOne
-    @JoinColumn(name = "category_id")
-    private Category category_id;
+    private Users users;
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Timestamp getDate_create() {
-        return date_create;
-    }
-
-    public void setDate_create(Timestamp date_create) {
-        this.date_create = date_create;
-    }
-
-    public Users getUsers_id() {
-        return users_id;
-    }
-
-    public void setUsers_id(Users users_id) {
-        this.users_id = users_id;
-    }
-
-    public Long getLikes() {
-        return likes;
-    }
-
-    public void setLikes(Long likes) {
-        this.likes = likes;
-    }
-
-    public Category getCategory_id() {
-        return category_id;
-    }
-
-    public void setCategory_id(Category category_id) {
-        this.category_id = category_id;
-    }
+    @ManyToOne
+    private Category category;
 }
--- src/main/java/com/casestudy/casestudy/repositories/blogs/CategoryRepository.java
+++ src/main/java/com/casestudy/casestudy/repositories/blogs/CategoryRepository.java
@@ -3,7 +3,9 @@ package com.casestudy.casestudy.repositories.blogs;
 
 import com.casestudy.casestudy.models.blogs.Category;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 
 }
